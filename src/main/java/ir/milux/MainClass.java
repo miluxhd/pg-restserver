ce9e5ccf105e527af81d8e1670ea70535690db23
milad zangeneh <miladx7268@gmail.com>
2017-09-15 17:51:47 +0430

    changing log configuration

--- src/main/java/ir/milux/MainClass.java
+++ src/main/java/ir/milux/MainClass.java
@@ -1,10 +1,13 @@
 package ir.milux;
 
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * Created by milux on 1/1/17.
  */
 public class MainClass {
     public static void main(String[] args) throws Exception {
+        PropertyConfigurator.configure("conf/log4j.properties");
         LightServer lightServer = new LightServer(8090);
         lightServer.start();
     }
--- src/main/java/ir/milux/PgConnection.java
+++ src/main/java/ir/milux/PgConnection.java
@@ -1,6 +1,9 @@
 package ir.milux;
 
 
+import org.apache.log4j.Logger;
+import sun.rmi.runtime.Log;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,9 +16,11 @@ import java.util.Properties;
  * Created by miluz on 9/15/17.
  */
 public class PgConnection {
+    private static Logger logger = Logger.getLogger(PgConnection.class);
     private static Connection INSTANCE;
     public static synchronized Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
         if (INSTANCE == null){
+            logger.info("initializing database connection...");
             Properties properties = new Properties();
             FileInputStream inputStream = new FileInputStream("conf/config.conf");
             properties.load(inputStream);
